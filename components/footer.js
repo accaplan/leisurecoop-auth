9e49c907485fa0e5ccc85ba6e6c24d65c5c75e6d
Ash Caplan <57927650+accaplan@users.noreply.github.com>
2021-11-18 00:44:51 +0100

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -5,11 +5,11 @@ export default function Footer() {
     <footer className="px-[25px] py-[30px] md:px-[35px] md:py-[30px] 2xl:py-[55px] 2xl:px-[65px] w-full relative z-10">
       <div className="flex flex-wrap">
         <div className="text-left">
-          <FancyLink destination="mailto:TalkToUs@L52.world" label="Contact" fast/>
+          <FancyLink destination="mailto:team@leisure.coop" label="Contact" fast/>
         </div>
 
         <div className="ml-auto text-right">
-          <FancyLink destination="https://www.instagram.com/l52world" label="Instagram" fast/>
+          <FancyLink destination="https://www.instagram.com/leisurecooperative" label="Instagram" fast/>
         </div>
       </div>
     </footer>
